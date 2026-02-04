9aa4e26329a1e2e0fb9c2e6aa7dddae39176e29e
MochiHLam <22110048@student.hcmute.edu.vn>
2026-02-04 23:21:47 +0700

    App configuration

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ChatApp</h1>
+      <p>Welcome to ChatApp!</p>
+    </div>
+  )
+}
+
+export default App
+
